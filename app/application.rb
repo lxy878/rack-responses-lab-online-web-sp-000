e16cd59a5313fb8bd2811cb0d02eb1f1ea876fa5
Xiaoyi Liu <lxy878@gmail.com>
2020-04-10 04:08:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,14 @@ class Application
   def call(env)
     resp = Rack::Responses.new
     now = Time.now
+
+    if now.hour < 12
+      resp.write 'Good Morning'
+
+    else
+      resp.write 'Good Afternoon'
+
+    end
   end
 
 end
